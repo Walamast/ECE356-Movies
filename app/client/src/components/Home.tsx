a2fbd02baa6245fefa0c24291b758a74e2f62669
Bruce <brucenguyen001@hotmail.com>
2021-04-15 22:16:32 -0400

    Started backend

--- app/client/src/components/Home.tsx
+++ app/client/src/components/Home.tsx
@@ -1,9 +1,73 @@
 import './Home.scss';
+import { useState, useEffect } from 'react';
 
 function Home() {
+  const [latestMovies, setLatestMovies] = useState([]);
+  const [topGrossMovies, setTopGrossMovies] = useState([]);
+  const [bornToday, setBornToday] = useState([]);
+
+  const getData = () => {
+    fetch("http://localhost:3001/api/latest", { method: 'GET' })
+    .then(function(response) {
+      return response.json();
+    }).then(function(json: any) {
+      setLatestMovies(json);
+    }).catch(function(err: any) {
+      console.error(err);
+    });
+
+    fetch("http://localhost:3001/api/gross", { method: 'GET' })
+    .then(function(response) {
+      return response.json();
+    }).then(function(json: any) {
+      setTopGrossMovies(json);
+    }).catch(function(err: any) {
+      console.error(err);
+    });
+
+    fetch("http://localhost:3001/api/birthday", { method: 'GET' })
+    .then(function(response) {
+      return response.json();
+    }).then(function(json: any) {
+      setBornToday(json);
+    }).catch(function(err: any) {
+      console.error(err);
+    });
+  }
+
+  useEffect(() => { getData() }, []);
+
   return (
-    <div>
-      <p style={{padding: 100, color: "white", textAlign: "center", fontWeight: 900}}>Coming soon.</p>
+    <div className="home">
+      <div className="row">
+        <div className="col-sm">
+          <h2>Latest Releases</h2>
+          <ul>
+            {latestMovies.map((val: any, key: any) => {
+              const url = "/movie/" + val.movieID;
+              return (<li key={key}><a href={url}>{val.originalTitle}</a></li>);
+            })}
+          </ul>
+        </div>
+        <div className="col-sm">
+          <h2>Highest Grossing (International)</h2>
+          <ul>
+            {topGrossMovies.map((val: any, key: any) => {
+              const url = "/movie/" + val.movieID;
+              return (<li key={key}><a href={url}>{val.originalTitle}</a></li>);
+            })}
+          </ul>
+        </div>
+        <div className="col-sm">
+          <h2>People Born Today</h2>
+          <ul>
+            {bornToday.map((val: any, key: any) => {
+              const url = "/people/" + val.personID;
+              return (<li key={key}><a href={url}>{val.name}</a></li>);
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/server/src/app.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import cors from 'cors';
+import * as mysql from 'mysql2';
+
+const app = express();
+app.use(cors());
+app.listen(3001, () => {
+    console.log("Express started on port 3001.");
+});
+
+const user = ''; // MySQL user
+const pass = ''; // MySQL pass
+const pool = mysql.createPool({
+    user: user,
+    host: 'marmoset04.shoshin.uwaterloo.ca',
+    password: pass,
+    database: 'project_35',
+});
+
+const runQuery = (query: string, res: any) => {
+    pool.getConnection(function(err: any, connection: any) {
+        if (err) {
+            console.error(err);
+            connection.release();
+            return;
+        }
+        connection.query(query,
+        (err: mysql.QueryError, result: any) => {
+            if (err) {
+                console.error(err);
+            } else {
+                res.send(result);
+            }
+            connection.release();
+        });
+    });
+}
+
+app.get('/api/latest', (req, res) => {
+    runQuery("select movieID, originalTitle from Movies where releaseDate <= curdate() order by releaseDate desc limit 10", res);
+});
+
+app.get('/api/gross', (req, res) => {
+    runQuery("select movieID, originalTitle from Movies order by grossInternational desc limit 10", res);
+});
+
+app.get('/api/birthday', (req, res) => {
+    runQuery("select personID, name from People where day(birthDate) = day(curdate()) and month(birthDate) = month(curdate()) limit 10", res);
+});
+
+// select movieID, title from Movies where releaseDate < curdate() order by releaseDate desc limit 10;
